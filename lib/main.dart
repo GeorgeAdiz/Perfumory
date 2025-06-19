1fbf508ac725b77dccd71dbd6853a2c26fcf5a73
GeorgeAdiz <georgegonzales664@gmail.com>
2025-06-19 10:21:17 +0800

    2nd commit

--- lib/main.dart
+++ lib/main.dart
@@ -4,34 +4,127 @@ void main() {
   runApp(MaterialApp(
     home: Scaffold(
       appBar: AppBar(
-        title: Text('Perfumory'),
-        backgroundColor: Colors.blueGrey[400],
+        title: Text(
+          'Perfumory',
+          style: TextStyle(
+            color: Colors.orange,
+            fontWeight: FontWeight.bold,
+            fontSize: 30,
+          ),
+        ),
+        backgroundColor: Colors.black,
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.center, // Center the column content
-        crossAxisAlignment: CrossAxisAlignment.center, // Center the text horizontally
+        // Align the elements vertically
+        crossAxisAlignment: CrossAxisAlignment.stretch, // Align elements to the left
         children: [
-          Center(
-            child: Text('Text 1', style: TextStyle(
-                color: Colors.blueGrey[700],
-                fontWeight: FontWeight.bold,
-                fontSize: 50
-            )),
+          // Container for Name
+          Container(
+            margin: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Colors.blueGrey.shade200, Colors.blueGrey.shade400],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10), // Padding for Name row
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Name:',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30,
+                  ),
+                ),
+                Text(
+                  'George Adiz',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30,
+                  ),
+                ),
+              ],
+            ),
           ),
-          Center(
-            child: Text('Text 2', style: TextStyle(
-                color: Colors.cyanAccent[700],
-                fontWeight: FontWeight.w800,
-                fontSize: 60
-            )),
+          SizedBox(height: 8), // Space between sections
+
+          // Container for Age
+          Container(
+            margin: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Colors.blueGrey.shade200, Colors.blueGrey.shade400],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10), // Padding for Age row
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Age:',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30,
+                  ),
+                ),
+                Text(
+                  '21',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30,
+                  ),
+                ),
+              ],
+            ),
           ),
-          Center(
-            child: Text('Text 3', style: TextStyle(
-                color: Colors.teal[700],
-                fontWeight: FontWeight.w900,
-                fontSize: 70
-            )),
+          SizedBox(height: 8), // Space between sections
+
+          // Container for Gender
+          Container(
+            margin: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Colors.blueGrey.shade200, Colors.blueGrey.shade400],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10), // Padding for Gender row
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Gender:',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30,
+                  ),
+                ),
+                Text(
+                  'Male',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30,
+                  ),
+                ),
+              ],
+            ),
           ),
+          SizedBox(height: 8),
         ],
       ),
     ),
