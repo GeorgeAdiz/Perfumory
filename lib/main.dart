c78ce1e07898ca29181753092f54ebdc6fcf08a5
GeorgeAdiz <georgegonzales664@gmail.com>
2025-07-07 09:46:46 +0800

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:my_app/pages/ListItem.dart';
 import 'pages/newperfume.dart';
 import 'pages/home.dart';
+import 'world_time/home_time.dart';
 
 void main() {
   runApp(MaterialApp(
     routes: {
-      '/': (context) => Listitems(),
+      '/': (context) => HomeTime(),
       '/newperfume': (context) => const Newperfume(),
       '/Listitems': (context) => Listitems(),
       '/Home': (context) => Home(),
+
+      // world time routes
+    
+
     },
   ));
 }
